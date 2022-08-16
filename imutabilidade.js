35b12d3a16900c6abe76b636c9137f6c42bafccf
JulioNunesDev <julionunes999@gmail.com>
2022-08-16 17:00:58 -0300

    feat: 'imutabilidade'

---
+++ imutabilidade.js
@@ -0,0 +1,15 @@
+const casa1 = {
+    cor: 'amarelo',
+    rua: 'AV. Brasil',
+    numero: 1
+}
+const casa2 = {...casa1, numero : 30};
+
+console.log(casa1);
+console.log(casa2);
+
+const lista1 = ['laranja', 'banana']
+const lista2 = [...lista1, 'maça']
+
+console.log(lista1);
+console.log(lista2);
